4ada614e8eb062918b37091a3d1b035cdb201465
Himanshu Anand <himanshuanand563@gmail.com>
2026-04-29 16:38:07 +0530

    components StartGame complited and app.jsx will done

--- Project-3/src/App.jsx
+++ Project-3/src/App.jsx
@@ -1,14 +1,20 @@
 
-import './App.css'
+import { useState } from 'react';
+import StartGame from './components/StartGame';
+import GamePlay from './components/GamePlay';
+
+import './App.css';
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
 
   return (
-    <div>
-
-    </div>
-  )
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
 }
 
-export default App
+export default App;
